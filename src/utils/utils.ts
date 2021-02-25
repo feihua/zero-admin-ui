b0e30797c10a4dabe97990fa2994aaf8ffd3b003
liufeihua <1002219331@qq.com>
2021-02-25 21:15:12 +0800

    数组对象转树对象

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -18,3 +18,26 @@ export const isAntDesignProOrDev = (): boolean => {
   }
   return isAntDesignPro();
 };
+
+/**
+ * 递归树
+ * @param {*} data 文件名
+ * @param {*} pid 父级id
+ * @param key
+ */
+export function tree(data: any, pid = 0, key = 'pid') {
+  const result = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const i in data) {
+    if (data[i][key] === pid) {
+      const temp = data[i];
+      const children = tree(data, data[i].id, key);
+      if (children.length) {
+        temp.children = children;
+      }
+      result.push(temp);
+    }
+  }
+
+  return result;
+}
