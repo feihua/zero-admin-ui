0e69f783fd99534b4ccb0cfd1b1ddae88bd61de2
koobe <koobe.liu@uaf.com.cn>
2023-12-18 11:14:00 +0800

    秒杀活动页面优化1

--- src/services/ant-design-pro/api.ts
+++ src/services/ant-design-pro/api.ts
@@ -6,7 +6,7 @@ import { request } from 'umi';
 export async function currentUser(options?: { [key: string]: any }) {
   return request<{
     data: API.CurrentUser;
-  }>('/api/sys/user/currentUser', {
+  }>('/api/sys/user/info', {
     method: 'GET',
     ...(options || {}),
   });
